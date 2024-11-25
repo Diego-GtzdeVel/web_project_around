4a0c429a139d1aefc873b90e80333cd7b1edebb7
Diego Gtz de Vel <gtzdevel@gmail.com>
2024-11-25 01:59:42 -0600

    Project 7

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tripleten web_project_around
+
+# Página web interactiva creada con HTML, CSS y Javascript
+
+# https://diego-gtzdevel.github.io/web_project_around/
--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,30 @@
+const popup = document.querySelector(".popup");
+const popupForm = document.querySelector(".popup__form");
+const nameInput = document.querySelector("#name");
+const aboutInput = document.querySelector("#about");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
-const nameInput = document.querySelector(".form__input-name");
-const aboutInput = document.querySelector(".form__input-about");
 const editButton = document.querySelector(".profile__edit");
 const closeButton = document.querySelector(".popup__close");
-const popup = document.querySelector(".popup");
 
-editButton.addEventListener("click", () => {
+function openPopup() {
   popup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent.trim();
+  aboutInput.value = profileDescription.textContent.trim();
+}
 
-  nameInput.placeholder = profileName.textContent;
-  aboutInput.placeholder = profileDescription.textContent;
-});
-
-closeButton.addEventListener("click", () => {
+function closePopup() {
   popup.classList.remove("popup_opened");
-});
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.firstChild.textContent = nameInput.value;
+  profileDescription.textContent = aboutInput.value;
+  closePopup();
+}
+
+editButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
+popupForm.addEventListener("submit", handleFormSubmit);
